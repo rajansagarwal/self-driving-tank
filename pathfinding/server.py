a663e842b174f054e89dd918622f890c1834a626
Rajan Agarwal <rajan.ag005@gmail.com>
2023-12-22 23:16:35 -0500

    adjustment for PID i wanted to experiment with based on convos with tesla engineers

--- pathfinding/server.py
+++ pathfinding/server.py
@@ -20,23 +20,23 @@ class PIDController:
         self.prev_error = error
         return output
 
-num_samples = 100
+def tune_pid(controller, target, iterations=100, dt=1):
+    for _ in range(iterations):
+        error = target - controller.prev_error
+        controller.calculate(error, dt)
 
-# Generating random distances
+num_samples = 100
 forward_distances = np.random.uniform(1, 10, num_samples)
 right_distances = np.random.uniform(1, 5, num_samples)
 left_distances = np.random.uniform(1, 5, num_samples)
 
-# Read data from a CSV file
 data = pd.read_csv('edge_distances.csv') 
 distances = data[['Distance Left', 'Distance Forwards', 'Distance Right']].values
 move_choice = data['Move Choice'].values
 
-# Convert move choice to one-hot encoding
 num_classes = 3
 move_choice_one_hot = tf.keras.utils.to_categorical(move_choice, num_classes)
 
-# Define a neural network model
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(10, activation='relu', input_shape=(3,)),
     tf.keras.layers.Dense(10, activation='relu'),
@@ -45,10 +45,8 @@ model = tf.keras.Sequential([
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 
-# Train the neural network model
 model.fit(distances, move_choice_one_hot[:, :3], epochs=20, batch_size=32)
 
-# Function to send a POST request
 def send_post_request(action, scaled, distance):
     url = "https://8f00-72-139-206-147.ngrok-free.app/"
     endpoints = ["/forward", "/right", "/left"]
@@ -58,7 +56,6 @@ def send_post_request(action, scaled, distance):
     requests.post(full_url, params=params)
     time.sleep(scaled)
 
-# Some arbitrary distances for prediction
 distances = np.array([[1.6, 1.2, 4.8], [7.1, 5.5, 4.2]])
 predictions = model.predict(distances)
     
@@ -67,23 +64,28 @@ Ki = 0.01
 Kd = 0.01
 pid_controller = PIDController(Kp, Ki, Kd)
 
+target = 0
+tune_iterations = 100
+tune_pid(pid_controller, target, tune_iterations)
+
 for i, pred in enumerate(predictions):
     move_forward, rotate_left, rotate_right, angle, distance = pred
     action = np.argmax([move_forward, rotate_left, rotate_right])
-    scaled_delay = int(distance)
+    scaled_delay = distance
     error = distance - scaled_delay
 
-    pid_output = pid_controller.calculate(error, dt=1) 
+    pid_output = pid_controller.calculate(error, dt=1)
+
+    error = max(min(error, 1), -1)
 
     scaled_delay += pid_output
 
-    # Adjust delays based on actions
     if action == 0:
-        scaled_delay *= 2
-    elif action == 1:
-        scaled_delay /= 4
+        scaled_delay *= 1.5
     else:
         scaled_delay /= 4
+
+    scaled_delay = max(min(scaled_delay, 10), 0.1)
 
     send_post_request(action, scaled_delay, distance)
     time.sleep(scaled_delay)
